00f55f3fdc3696924f7ededf8c39deadf03aeff2
rKeller123 <raphael.keller@students.unibe.ch>
2025-02-19 09:56:21 +0100

    bonus

--- Serie 1/solution.R
+++ Serie 1/solution.R
@@ -26,6 +26,11 @@ for (i in 2:4) {
 }
 lines(x, dcauchy(x), col = "blue")
 
+legend(-6, 0.4,
+       c("Student-Dichten für df = 2, 5, 10, 20", "Standard Cauchy Dichte"),
+       lty = 1,
+       col = c("black", "blue"))
+
 plot(
   x, pt(x, df[1]),
   main = "Student- Verteilungen & standard Cauchy-Verteilung",
@@ -37,7 +42,25 @@ for (i in 2:4) {
 }
 lines(x, pcauchy(x), col = "blue")
 
+legend(-6, 1,
+       c("Student-Verteilungen für df = 2, 5, 10, 20", "Standard Cauchy Verteilung"),
+       lty = 1,
+       col = c("black", "blue"))
+
 
 # e
 
-plot(pbinom(x, 10, 0.3), type = "s")
+n <- 10
+p <- 0.3
+
+plot(pbinom(0:n, n, p), type = "s",
+     main = "Binomialverteilung mit n = 10, p = 0.3",
+     xlab = "x",
+     ylab = NA)
+points(dbinom(0:n, n, p), pch = 20, col = "darkgreen", cex = 1.5)
+
+legend(1, 1,
+       c("P[X <= x]", "P[X = x]"),
+       col = c("black", "darkgreen"),
+       lty = c(1, NA),
+       pch = c(NA, 20))
